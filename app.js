3802039a0c749fa527dd9721ff69faf4dc32c5ca
amritansh-mishra <mamritansh1911@gmail.com>
2025-05-22 19:30:09 +0530

    dA-2

--- app.js
+++ app.js
@@ -16,6 +16,26 @@ app.get('/', (req, res) => {
     res.render('index');
     });
 
+app.get('/login', (req, res) => {
+    res.render('login');
+    });
+app.get('/profile',isLoggnedIn, async(req, res) => {
+   let user= await userModel.findOne({email: req.user.email}).populate("posts"); //will show what's written / if won't use populate the field will remain empty
+   
+    res.render('profile', {user});
+    });
+app.post('/post',isLoggnedIn, async(req, res) => {                        //hmseha post tbhi hoga jab apn logged in honge
+   let user= await userModel.findOne({email: req.user.email});
+   let {content} = req.body;
+    let post =  await postModel.create({
+        user: user._id,
+        content: content
+    });
+    user.posts.push(post.id);
+    await user.save();
+    res.redirect("/profile")
+    });
+
 app.post('/register', async(req, res) => {
  let {email, password, username, name, age}= req.body; 
 
@@ -41,4 +61,44 @@ app.post('/register', async(req, res) => {
         })
 
 });
+app.post('/login', async(req, res) => {
+ let {email, password}= req.body; 
+
+ let user = await userModel.findOne({email});
+    if(!user){
+            return res.status(500).send("Something went wrong");
+    };
+        //checking the password
+        bcrypt.compare(password, user.password), function(err, result){
+                if(result){
+                        let token = jwt.sign({email: email, userid: user._id}, "shhhh");
+                        res.cookie("token", token);
+                        res.redirect("/profile");
+                }
+                else{
+                        res.redirect('/login');
+                }
+        }
+        
+});
+
+app.get('/logout', (req, res) => {
+    res.cookie("token", "");
+    res.redirect('/login');
+    });
+    // MiddleWare to check if user is logged in (protecting the route)
+function isLoggnedIn(req, res, next) {
+    if (!req.cookies.token || req.cookies.token === "") {
+        return res.send("You must be logged in");
+    }
+
+    jwt.verify(req.cookies.token, "shhhh", (err, decoded) => {
+        if (err) {
+            return res.status(403).send("Invalid token");
+        }
+
+        req.user = decoded; // use the decoded value from the callback
+        next();
+    });
+}
 app.listen(3000);
